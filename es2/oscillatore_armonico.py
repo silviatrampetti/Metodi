ab8b69197ba3ab85c72c92ec478bf2b8c6ff8563
silviatrampetti <st010012@infolab01>
2022-11-02 17:06:27 +0100

    modifica

--- es2/oscillatore_armonico.py
+++ es2/oscillatore_armonico.py
@@ -4,46 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
-def periodo(m,k,x):
-    return np.sqrt(8*m)*integrate.simpson(1/(np.sqrt(0.5*m*np.gradient(x)**2*k*x**6)),x)
+def V_func(x,k):
+    return k*x**6
 
-xx0 = np.arange(10)
-kk=10
-mm=20
-T=np.array([0])
+k=1
+m=10
+cost = np.sqrt(8*m)
 
-for i in range(len(xx0)):
-    T=np.append(T,periodo(mm,kk,xx0[i]))
+x0=np.empty(0) #punto da cui parte
+T=np.empty(0) #periodo
 
+for i in np.arange(1,30,0.001):
+    x0=np.append(x0,i)
+    x=np.arange(0,i,0.001)
+    V0=V_func(i,k)
+    V=V_func(x,k)
+    T=np.append(T,cost*integrate.simpson(1/np.sqrt(V0-V),x))
 
-
-
-
-
-
-'''
-def V0_f(m,v,V):
-    return 0.5*m*v**2 + V
-
-def sqrt_f(V0,V):
-    return np.sqrt(V0-V)
-
-kk=0.5
-mm=1
-
-x0 = np.arange(-5,5)
-xx = 
-vv=np.gradient(xx)
-V=V_f(kk,xx)
-V0=V0_f(mm,vv,V)
-f=sqrt_f(V0,V)
-T=np.array([0])
-
-for i in range(1,len(xx)):
-    T=np.append(T,np.sqrt(8*mm)*integrate.simpson(f[:i],xx[:i]))
-
-plt.plot(xx,T)
-plt.xlabel('x')
-plt.ylabel('V')
+plt.plot(x[:len(T)],T)
+plt.xlabel('x0')
+plt.ylabel('T')
 plt.show()
-'''
